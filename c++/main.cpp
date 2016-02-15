8ccc08196d1d3774719a8c630023ba8cf2f31403
hetec <hebner.tec@gmail.com>
2016-02-15 11:51:53 +0100

    Adapt the number of loops and compile with optimization flag

--- c++/main.cpp
+++ c++/main.cpp
@@ -20,11 +20,7 @@ double division(double a, double b) {
 }
 
 long absolute(long a) {
-<<<<<<< HEAD
-  return (a) < 0 ? ((a) * -1) : (a);  
-=======
   return a < 0 ? (a * -1) : a;
->>>>>>> 8c2080953da4d84190ad90bf496c2b29ec84aac4
 }
 
 long printValue(long value, long inner) {
@@ -34,25 +30,17 @@ long printValue(long value, long inner) {
   return inner;
 }
 
-<<<<<<< HEAD
 long innerLoop(long counter, long limit) {
   long inner = 0;
   while (counter < limit) {
     counter++; 
     inner = printValue(counter, inner);
-=======
-void innerLoop(long counter, long limit) {
-  while (counter <= limit) {
-    counter++;
-    printValue(counter);
->>>>>>> 8c2080953da4d84190ad90bf496c2b29ec84aac4
   }
   return inner;
 }
 
 
 int main() {
-  
   long test;
   long counter = 0;
   double limit = 10000;
